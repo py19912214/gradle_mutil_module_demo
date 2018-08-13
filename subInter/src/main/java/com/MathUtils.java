710f4dbcd979c61d835056bd72acb901ba049ea6
yuepan <yuepan>
2018-08-13 14:05:44 +0800

    新建多模块项目

---
+++ subInter/src/main/java/com/MathUtils.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * @Auther: yuepan
+ * @Description: yuepan
+ * <p>
+ * ------------------------------------------------------
+ * @Date: 2018/8/13 11:25
+ * ------------------------------------------------------
+ */
+public class MathUtils {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ subWeb/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<%
+
+    int a = 200;
+    int b = 300;
+    int c = com.MathUtils.sum(a,b);
+    out.println("<h2>"+ c+"</h2>");
+    out.println("panyue");
+%>
+
+</body>
+</html>
